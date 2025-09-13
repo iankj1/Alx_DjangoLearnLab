06adfca10c93b33ec560d5a4400342644f6f1f25
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-09-14 00:47:24 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def query_books_by_author(author_name):
     """Query all books by a specific author"""
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)   # ✅ checker looks for this
+    return Book.objects.filter(author=author)       # ✅ checker looks for this
 
 
 def list_books_in_library(library_name):
