849a1958ed038da1e76187bf707f8ea7c197a0f0
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 20:34:32 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # home or other app urls...
     path('', views.home, name='home'),
+
+    # Registration
+    path('register/', views.register, name='register'),
+
+    # Profile
+    path('profile/', views.profile, name='profile'),
+
+    # Login and Logout using Django's built-in views
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
 ]
