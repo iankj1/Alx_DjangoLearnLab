7461fd69c0f3cccb25e8f19e7fd90de79de9d2a3
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 21:09:06 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -46,3 +46,27 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         instance.profile.save()
+        
+class Comment(models.Model):
+    """
+    Comment left by a User on a Post.
+    - post: FK to Post (many comments per post).
+    - author: FK to User (the commenter).
+    - content: text of the comment.
+    - created_at / updated_at: timestamps.
+    """
+    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']  # oldest first for readability
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+    def get_absolute_url(self):
+        # After creating / editing / deleting a comment we usually return to the post detail
+        return reverse('post-detail', kwargs={'pk': self.post.pk})
