f4aa9aeb2321aae4661ff586dac856e77d85c2bc
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 20:55:39 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -34,3 +34,50 @@ def logout_view(request):
 @login_required
 def profile_view(request):
     return render(request, 'blog/profile.html')
+    
+#POST CRUD VIEW
+class PostListView(ListView):
+    """Public view: List all posts"""
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+class PostDetailView(DetailView):
+    """Public view: View a single post"""
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    """Create a new post (only logged-in users)"""
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Edit a post (only by its author)"""
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    login_url = 'login'
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Delete a post (only by its author)"""
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+    login_url = 'login'
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
