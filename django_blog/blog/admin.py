b582657ff0bf2fb08c74dc575044e8db70d12434
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 21:14:15 +0300

    Update admin.py

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Profile
+from .models import Comment
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'bio')
+from django.contrib import admin
+from .models import Comment
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    search_fields = ('author__username', 'content', 'post__title')
