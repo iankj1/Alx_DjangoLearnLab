b06c292b2cdf62742b2412b733d5f89b3c743515
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 20:32:42 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -9,3 +11,24 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    """
+    Extends the built-in User model with optional fields.
+    Kept minimal: bio and avatar (profile picture path).
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return f'Profile: {self.user.username}'
+
+# Signal to create or save profile automatically when user is created
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
