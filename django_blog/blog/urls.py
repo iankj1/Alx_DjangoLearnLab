a31b2d63035ac9a14c7954617ed69fdf8f0efb92
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 21:25:23 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+
+      # tag & search
+    path('tags/<str:tag_name>/', views.PostsByTagView.as_view(), name='posts-by-tag'),
 ]
