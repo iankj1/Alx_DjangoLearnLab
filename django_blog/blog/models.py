95adefb4e5b2e362f44994017b81b4d7cc847588
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 21:19:35 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,28 +3,42 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.urls import reverse
+from django.utils.text import slugify
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=60, unique=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts-by-tag', kwargs={'slug': self.slug})
+
 
 class Post(models.Model):
-    """
-    Blog post model.
-    - author: FK to User (one user can have many posts).
-    - title, content: user-editable.
-    - created_at, updated_at: timestamps.
-    """
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')   # <-- new
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']  # newest first
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        # used by generic CreateView/UpdateView redirect on success
         return reverse('post-detail', kwargs={'pk': self.pk})
 
 class Profile(models.Model):
