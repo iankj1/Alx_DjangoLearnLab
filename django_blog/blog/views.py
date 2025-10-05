b15f2a4bc560e7420a6a0b605f2f70d32b85d553
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-05 20:34:06 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-def home(request):
-    posts = Post.objects.all().order_by('-published_date')
-    return render(request, 'blog/home.html', {'posts': posts})
+def register(request):
+    """
+    Handle user registration. On success, redirect to login.
+    """
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Account created. You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    """
+    Profile view: allow authenticated users to view and edit their profile.
+    Uses two forms (UserUpdateForm and ProfileUpdateForm) to update both User and Profile.
+    """
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your profile was updated.')
+            return redirect('profile')  # PRG pattern
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'blog/profile.html', context)
