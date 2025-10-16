7c91d3ddbc37c4582093012408a30fbe8eed39e9
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-10-16 14:09:36 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,18 +1,21 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import AbstractUser 
 
 def user_profile_picture_path(instance, filename):
     return f"profile_pictures/user_{instance.id}/{filename}"
 
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(
-        upload_to=user_profile_picture_path, blank=True, null=True
+    bio = models.TextField(blank=True, null=True) 
+    profile_picture = models.ImageField(          
+        upload_to=user_profile_picture_path,
+        blank=True,
+        null=True
     )
-    # followers: users who follow this user
-    followers = models.ManyToManyField(
-        "self", symmetrical=False, related_name="following", blank=True
+    followers = models.ManyToManyField(           
+        "self",
+        symmetrical=False,
+        related_name="following",
+        blank=True
     )
 
     def __str__(self):
